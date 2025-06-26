2db8cb4f8649baa9f8ccbeef52b05256f3b656d1
aulia-hn <aulia.edme@gmail.com>
2025-06-26 18:09:21 +0200

    26 juin 25_2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Le nom exact du dépôt = "aulia_website"
 export default defineConfig({
+  base: '/aulia_website/',
   plugins: [react()],
 })
